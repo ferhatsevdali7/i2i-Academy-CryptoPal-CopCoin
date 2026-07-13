a84f1f9c2f1c14ec21f3cc7acaea4d5458146974
Ferhat Sevdalı <ferhat7sevdali7@gmail.com>
2026-07-14 02:45:07 +0300

    İlk Commit: Spring Boot proje iskeleti ve bagimliliklar kuruldu | Initial Commit: Spring Boot project skeleton and dependencies initialized

---
+++ src/main/java/com/i2i/cryptopal/CryptopalApplication.java
@@ -0,0 +1,13 @@
+package com.i2i.cryptopal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptopalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptopalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/i2i/cryptopal/CryptopalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i2i.cryptopal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptopalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
