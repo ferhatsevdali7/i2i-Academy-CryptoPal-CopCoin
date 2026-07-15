dad6f1fba25c13ca480e20cb6840dd72e224dcb2
Emir Şefik Temel <temelemir163@gmail.com>
2026-07-15 22:37:13 +0300

    feat: cüzdan yönetim modülü geliştirildi ve veritabanı şeması entity yapısına uygun hale getirildi

--- src/main/java/com/i2i/cryptopal/CryptopalApplication.java
+++ src/main/java/com/i2i/cryptopal/CryptopalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling; // 1. BU IMPORT'U EKLEDİK
 
 @SpringBootApplication
-@EnableScheduling // 2. BU ETİKETİ EKLEDİK: Arka plandaki zamanlanmış görevleri tetikler -emir- simüle fiyat trendlerini oluşturmak için kullanacağız
+@EnableScheduling // 2. BU ETİKETİ EKLEDİK: Arka plandaki zamanlanmış görevleri tetikler -- simüle fiyat trendlerini oluşturmak için kullanacağız
 public class CryptopalApplication {
     public static void main(String[] args) {
         SpringApplication.run(CryptopalApplication.class, args);
